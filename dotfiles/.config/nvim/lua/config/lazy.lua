8a5e8e82dd246be37fd08fc335680f5bca4d193f
this-is-tobi <this-is-tobi@proton.me>
2026-07-15 03:11:30 +0200

    feat(nvim): enable mini.move and add claudecode/helm/yaml extras
    mini.move binds Opt+Up/Down to move the current line or selection,
    matching VS Code. Also pull in the claudecode, helm and yaml LazyVim
    extras to round out the language/tooling coverage.

--- dotfiles/.config/nvim/lua/config/lazy.lua
+++ dotfiles/.config/nvim/lua/config/lazy.lua
@@ -11,12 +11,16 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.ai.claudecode" },
+    { import = "lazyvim.plugins.extras.editor.mini-move" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.helm" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     -- import/override with your plugins
     { import = "plugins" },
---
+++ dotfiles/.config/nvim/lua/plugins/mini-move.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-mini/mini.move",
+    opts = {
+      mappings = {
+        up = "<M-Up>",
+        down = "<M-Down>",
+        line_up = "<M-Up>",
+        line_down = "<M-Down>",
+      },
+    },
+  },
+}
